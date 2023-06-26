456544cbb58b5ab2b60a18743bcf6ce25e05f652
arijit ghosh <subhasghosh196@outlook.com>
2023-06-26 19:18:52 +0530

    fixed bugs

--- source code/main.py
+++ source code/main.py
@@ -7,7 +7,7 @@ files.sort(key=lambda x: os.path.getmtime(x))
 # print(files) # list index 0 hold the oldest file
 digits=int(log10(len(files)))+1
 
-for i in range(1,len(files)):
+for i in range(len(files)-1):
     newfile_name=f"{str(i).zfill(digits)}_{files[i]}"
     os.rename(files[i],newfile_name)
     # print(newfile_name)
